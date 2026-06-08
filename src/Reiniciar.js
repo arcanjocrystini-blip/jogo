f30b6928eef33d660670dd4a95728e1ca7a40309
Brunna Hadassa Silva <ribeirolindalva48@gmail.com>
2026-06-08 11:43:47 -0300

    Limpar histórico e destaque ao reiniciar

--- src/Reiniciar.js
+++ src/Reiniciar.js
@@ -1,10 +1,9 @@
-export function BotaoReiniciar({setQuadrados, setEstado, setStatus}) {
- 
- function reiniciarJogo() {
-    setQuadrados(Array(9).fill(null));
-    setEstado(false);
-    setStatus(null);
-  }
 
-  return <><div><button onClick={reiniciarJogo}>Reiniciar</button></div></>
+function reiniciarJogo() {
+  setQuadrados(Array(9).fill(null));
+  setEstado(false);
+  setStatus(null);
+
+  setHistorico([]);
+  setLinhaVencedora([]);
 }
