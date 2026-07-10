bb638c1987f4a6f03f8a6febcf68783cb51cfd2a
ribeirolindalva48-rgb <ribeirolindalva48@gmail.com>
2026-07-10 14:14:26 -0300

    Update Reiniciar.js

--- src/Reiniciar.js
+++ src/Reiniciar.js
@@ -1,11 +1,4 @@
-export function BotaoReiniciar({ setQuadrados, setEstado, setStatus }) {
-
-  function reiniciarJogo() {
-    setQuadrados(Array(9).fill(null));
-    setEstado(false);
-    setStatus(null);
-  }
-
+export function BotaoReiniciar({ reiniciarJogo }) {
   return (
     <button onClick={reiniciarJogo}>
       Reiniciar Jogo
